b7bb6a4c10aaf0590a3d1f89515750b4b14c04f0
hoelbing <carsten@hoelbing.net>
2016-07-04 17:10:23 +0200

    second init

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # BioBelloMage2Theme
+
+Dies ist ein Magento 2 Template. Dies wurde im Rahmen der Vorlesung "Web Shop Projekt" an der EAH-Jena im Sommersemester 2016 erstellt.
---
+++ app/design/frontend/Hoelbing/BioBelloMage2Theme/registration.php
@@ -0,0 +1,7 @@
+<?php
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::THEME,
+    'frontend/Hoelbing/BioBelloMage2Theme',
+    __DIR__
+);
